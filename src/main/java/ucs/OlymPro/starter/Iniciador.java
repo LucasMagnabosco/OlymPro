e3bfd502c15dbdf5ce433e0a00c79c3f56dc510a
LucasMagnabosco <lucasmagnabosco@live.com>
2024-10-20 23:41:21 -0300

    Base

---
+++ src/main/java/ucs/OlymPro/starter/Iniciador.java
@@ -0,0 +1,15 @@
+package ucs.OlymPro.starter;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import ucs.OlymPro.view.MainScreen;
+
+public class Iniciador {
+	
+	public static void main(String[] args) {
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("dados");
+		MainScreen frame = new MainScreen();
+		frame.setVisible(true);
+	}
+}
